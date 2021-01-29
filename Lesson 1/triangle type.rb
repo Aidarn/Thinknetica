ef5c408b2ad672ef2cd9eaa0a021b0cf034978e3
Aidarn <77989239+Aidarn@users.noreply.github.com>
2021-01-29 15:09:11 +0500

    correction

--- Lesson 1/triangle type.rb
+++ Lesson 1/triangle type.rb
@@ -1,22 +1,22 @@
 puts "Для определения типа треуголника введите длинны его сторон"
 puts "Введите первое число"
-  a = gets.chomp
+a = gets.chomp
 puts "Введите второе число"
-  b = gets.chomp
+b = gets.chomp
 puts "Введите третье число"
-  c = gets.chomp
+c = gets.chomp
 if
   a.to_i-b.to_i==0 && a.to_i - c.to_i==0
-    puts "Этот треугольник равносторонний"
-  elsif
-    a==b || a==c || b==c 
-    puts "Этот треугольник равнобедренный"
-  elsif
-    (c.to_i*c.to_i) == (a.to_i*a.to_i + b.to_i*b.to_i)
-      puts "Это прямой треугольник"
-    elsif a.to_i**2 == c.to_i**2 + b.to_i**2
-      puts "Это прямой треугольник"
-    elsif b.to_i**2 == c.to_i**2 + a.to_i**2
-      puts "Это прямой треугольник"
-    else puts "Это обычный треугольник"
-  end
+  puts "Этот треугольник равносторонний"
+elsif
+  a==b || a==c || b==c 
+  puts "Этот треугольник равнобедренный"
+elsif
+  (c.to_i*c.to_i) == (a.to_i*a.to_i + b.to_i*b.to_i)
+  puts "Это прямой треугольник"
+elsif a.to_i**2 == c.to_i**2 + b.to_i**2
+  puts "Это прямой треугольник"
+elsif b.to_i**2 == c.to_i**2 + a.to_i**2
+  puts "Это прямой треугольник"
+else puts "Это обычный треугольник"
+end
