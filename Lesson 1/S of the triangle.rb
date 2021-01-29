f8fb8b0696ec138231cd2ad9bd752e26f44c0936
Aidarn <77989239+Aidarn@users.noreply.github.com>
2021-01-29 14:48:36 +0500

    correction

--- Lesson 1/S of the triangle.rb
+++ Lesson 1/S of the triangle.rb
@@ -1,7 +1,7 @@
 puts "Для нахождения площади треугольника:"
 puts "Введите длинну основания треугольника"
-  footing = gets.chomp
+footing = gets.chomp.to_i 
 puts "И высоту треугольника"
-  height = gets.chomp
-  S = (1/2.0) * footing.to_i * height.to_i
-puts "Площадь треугольника = #{S}"
+height = gets.chomp.to_i
+s = (1/2.0) * footing * height
+puts "Площадь треугольника = #{s}"
