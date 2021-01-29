305bcfed8775c0b3d6e0c4ce7869c266c73d7819
Aidarn <77989239+Aidarn@users.noreply.github.com>
2021-01-29 14:52:31 +0500

    correction

--- Lesson 1/ideal weight.rb
+++ Lesson 1/ideal weight.rb
@@ -1,11 +1,10 @@
 puts "Привет, как тебя зовут?"
-  name = gets.chomp
+name = gets.chomp
 puts "А какой у тебя рост?"
-  height = gets.chomp
-  weight = (height.to_i - 110) * 1.15
-  if weight > 0
-    puts "#{name}, твой идеальный вес примерно #{weight}"
-  else 
-    puts "#{name}, Ваш вес уже оптимальный"
-  end
-  
+height = gets.chomp.to_i
+weight = (height - 110) * 1.15
+if weight > 0
+  puts "#{name}, твой идеальный вес примерно #{weight}"
+else 
+  puts "#{name}, Ваш вес уже оптимальный"
+end
