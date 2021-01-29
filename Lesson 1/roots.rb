6c303fdd7a0b86a0b1b088ba26f09c752186bf72
Aidarn <77989239+Aidarn@users.noreply.github.com>
2021-01-29 15:06:04 +0500

    correction

--- Lesson 1/roots.rb
+++ Lesson 1/roots.rb
@@ -1,20 +1,19 @@
 puts "Введите первое число"
-  a = gets.chomp
+a = gets.chomp.to_i
 puts "Введите второе число"
-  b = gets.chomp
+b = gets.chomp.to_i
 puts "Введите третье число"
-  c = gets.chomp
-  k = -1
-  D = (b.to_i**2.0) - (4*a.to_i*c.to_i)
-  if D > 0 
-    C = Math.sqrt(D.to_i)
-    root1 = (k*b.to_i + C.to_i)/(2*a.to_i)
-    root2 = (k*b.to_i - C.to_i)/(2*a.to_i)
-    puts "Дискриминант равен: #{D} и имеет корни #{root1} и #{root2}"
-  elsif D < 0
-    puts "Дискриминант равен: #{D} и не имеет корней"
-  elsif D==0
-    C = Math.sqrt(D.to_i)
-    root3 = (-b.to_i + C.to_i)/(2*a.to_i)
-    puts "Дискриминант равен: #{D} и имеет 2 одиннаковых корня #{root3}"
-  end
+c = gets.chomp.to_i
+d = (b**2.0) - (4 * a * c)
+if d > 0 
+  c2 = Math.sqrt(d)
+  root1 = (- b + c2)/(2 * a)
+  root2 = (- b - c2)/(2 * a)
+  puts "Дискриминант равен: #{d} и имеет корни #{root1} и #{root2}"
+elsif d < 0
+  puts "Дискриминант равен: #{d} и не имеет корней"
+elsif d == 0
+  c2 = Math.sqrt(d)
+  root3 = (- b + c2)/(2 * a)
+  puts "Дискриминант равен: #{d} и имеет 2 одиннаковых корня #{root3}"
+end
