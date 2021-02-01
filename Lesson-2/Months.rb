18e7804ac7e3fe715ecc3d64f19c2118c032975a
Aidarn <aidar.nig775511@gmail.com>
2021-02-01 22:26:00 +0500

    Rename

--- Lesson-2/Months.rb
+++ Lesson-2/Months.rb
@@ -1,8 +1,8 @@
-mounths = {
+months = {
   "January" => 31, "February" => 28, "March" => 31, "April" => 30, "May" => 31, "June" => 30,
   "July" => 31, "August" => 31, "September" => 30, "October" => 31, "November" => 31, "December" => 31
 }
-mounths.each { |mouth, days| 
+months.each { |mouth, days| 
   if days == 30
   puts mouth
   end 
