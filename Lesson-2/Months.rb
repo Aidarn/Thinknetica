f19fa36d1fab228227a51260fd1494b991998691
Aidarn <aidar.nig775511@gmail.com>
2021-02-03 16:08:17 +0500

    bug fixes

--- Lesson-2/Months.rb
+++ Lesson-2/Months.rb
@@ -2,8 +2,4 @@ months = {
   "January" => 31, "February" => 28, "March" => 31, "April" => 30, "May" => 31, "June" => 30,
   "July" => 31, "August" => 31, "September" => 30, "October" => 31, "November" => 31, "December" => 31
 }
-months.each { |month, days| 
-  if days == 30
-  puts month
-  end 
-  }
+months.each { |month, days| puts month if days == 30 }
